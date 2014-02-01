2c8a67e4f17cca4dad6afcdf2ccd4c752322971a
John Lempka <johnlempka@gmail.com>
2014-02-01 09:44:45 -0500

    Should have fixed redis again?

--- app.js
+++ app.js
@@ -11,11 +11,11 @@ var redis = require('redis');
 var url = require('url');
 var redisURL = url.parse(process.env.REDISCLOUD_URL);
 
-console.log("Redis url: process.env.REDISCLOUD_URL");
+console.log("Redis url: " + redisURL);
 
 var client = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
 if (redisURL.auth) {
-  rclient.auth(redisURL.auth.split(":")[1]);
+  client.auth(redisURL.auth.split(":")[1]);
 }
 
 var options = {title: 'frames'};
