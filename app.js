de6f48462983395013d824d258e12d5fdbc8999e
John Lempka <johnlempka@gmail.com>
2014-02-01 09:36:55 -0500

    Logging

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ var redis = require('redis');
 var url = require('url');
 var redisURL = url.parse(process.env.REDISCLOUD_URL);
 
+console.log("Redis url: process.env.REDISCLOUD_URL");
+
 var rclient = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
 if (redisURL.auth) {
   rclient.auth(redisURL.auth.split(":")[1]);
